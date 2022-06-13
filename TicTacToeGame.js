88f4210ba997d64883c7520f10c032563c4ec9f6
nathan12213213 <107374567+nathan12213213@users.noreply.github.com>
2022-06-12 18:01:30 -0600

    Update TicTacToeGame.js

--- TicTacToeGame.js
+++ TicTacToeGame.js
@@ -1,1 +1,26 @@
+public class TicTacToe {
 
+	public static void main(String[] args){
+
+ 	char[][] gameBoard = {{' ', '|', ' ', '|', ' '},
+						  {'_', '+', '_', '+', ' '},
+						  {' ', '|', ' ', '|', ' '},
+						  {'_', '+', '_', '+', ' '},
+						  {' ', '|', ' ', '|', ' '}};
+
+		printGameBoard(gameBoard);
+    
+	}
+	
+   public static void printGameBoard(char[][] gameBoard){
+	   for(char[] row : gameBoard){
+		   for(char c : row){
+			   System.out.print(c);
+		   }
+		   System.out.println();
+	   }
+   }
+
+
+	
+}
